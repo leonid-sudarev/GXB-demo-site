9196c086d96f89286f3bb2d566368a71923c0186
scrag <scrag1987@gmail.com>
2020-06-02 14:02:32 +0600

    initial commit

---
+++ src/main/java/com/GXB/GXBdemosite/GxbDemoSiteApplication.java
@@ -0,0 +1,13 @@
+package com.GXB.GXBdemosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GxbDemoSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GxbDemoSiteApplication.class, args);
+	}
+
+}
